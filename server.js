23e11f2cd61d323adc7bd9b61d54504051469b43
Tomo Sindberg <tsindberg@gmail.com>
2015-07-26 20:31:19 -0700

    updates

--- server.js
+++ server.js
@@ -8,11 +8,26 @@
 //   One of the elements should be a link to an external page.
 //   Things to research: node.js, callbacks, the fs module, the http module.
 
-var http = require("http");
+var http = require('http');
+var fs = require('fs');  //fs = file system, needed for files
+var connect = require('connect');
+var commonHeaders = {'Content-Type': 'text/html'};
 
-http.createServer(function(request, response){
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<h1> This is a Simple Server!</h1>");
-  response.end();
-}).listen(3000);
+
+var server = http.createServer(function(request, response){
+  if (request.method == 'GET' && request.url =='/') {
+    fs.readFile('index.html', function(error, page){
+      response.writeHead(200, commonHeaders);
+      response.write(page);
+      response.end();
+    });
+  }
+  else {
+    response.writeHead(404, commonHeaders);
+    response.write("Error 404 Page Not Found.");
+    response.end();
+  }
+});
+server.listen(3000);
 console.log("Server is now running...");
+
